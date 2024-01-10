6c6c105041795e4614ae646d32d13b084c047e6a
Kent Hirv <59705986+kenthirv0@users.noreply.github.com>
2024-01-10 15:38:23 +0200

    Add files via upload

---
+++ hindeline.too.khirv.4.py
@@ -0,0 +1,56 @@
+#iseseisev töö
+#K.Hirv
+#10.01.24
+
+
+def pronksikarva_summa(f):
+    kassa = 0
+    fail = open(f)
+    for mynt in fail:
+        if int(mynt)<10:
+            print(mynt, end="")
+            kassa += int(mynt)
+    print("\nKassas: ", kassa)
+
+
+pronksikarva_summa("mündid.txt")
+
+#-------------------------------
+kylalised = 8
+def tervitus(n):
+    print('Võõrustaja: "Tere!"')
+    print(f"Täna {n} kord tervitada!")
+    print('Külaline: "Tere, suur tänu kutsumast!"')
+
+for i in range(kylalised):
+    tervitus(i+1)
+
+
+#-------------------------------
+def eelarve(kylalised):
+    summa = kylalised * 10 + 55
+    return summa
+
+palju = int(input("Mitu kutsutud: "))
+palju2 = int(input("Mitu tuleb: "))
+print(f"Maksimaalne eelarve: {eelarve(palju)}€")
+print(f"Minimaalne eelarve: {eelarve(palju2)}€")
+
+
+#-------------------------------
+def mahlapakkide_arv(kg):
+    pakid = round(kg * 0.4 / 3)
+    return pakid
+kogus = float(input("Õunte kogus: "))
+print(mahlapakkide_arv(kogus))
+
+
+#-------------------------------
+def banner(t):
+    print(t.upper())
+
+ask = int(input("Mitu korda: "))
+ask2 = input("Reklaamlause: ")
+
+for i in range(ask):
+    banner(ask2)
